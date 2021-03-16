027e88b8c945dd394a148946f6c5c86eac095cfa
Alexander Polyakov <devops.edc@gmail.com>
2021-03-16 20:43:42 +0300

    Fixed structure of file outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,7 @@ resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
   {
     maven-dns = aws_instance.maven.public_dns,
-    maven-ip = aws_instance.maven.public_ip
-  },
-  {
+    maven-ip = aws_instance.maven.public_ip,
     tomcat-dns = aws_instance.tomcat.public_dns,
     tomcat-ip = aws_instance.tomcat.public_ip
   }
