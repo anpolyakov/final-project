2bb575b4b8b0c3e6b29ab58864bdbdaf790d9843
Alexander Polyakov <devops.edc@gmail.com>
2021-03-16 20:38:37 +0300

    Fixed syntax outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
   {
-    maven-dns = aws_instance.maven.public-dns,
+    maven-dns = aws_instance.maven.public_dns,
     maven-ip = aws_instance.maven.public_ip
   },
   {
-    tomcat-dns = aws_instance.tomcat.public-dns,
+    tomcat-dns = aws_instance.tomcat.public_dns,
     tomcat-ip = aws_instance.tomcat.public_ip
   }
   )
